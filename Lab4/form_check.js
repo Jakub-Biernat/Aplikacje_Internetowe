2818c55a083d186b24e14120018723907f014017
Jakub-Biernat <k.biernat@vp.pl>
2026-04-03 14:43:20 +0200

    Add podpunkty 21-25

--- Lab4/form_check.js
+++ Lab4/form_check.js
@@ -1,40 +1,35 @@
+function validate(formularz){
+  if(!checkStringAndFocus(formularz.elements["f_imie"], "Podaj imię!", isWhiteSpaceOrEmpty)) return false;
+
+  if(!checkStringAndFocus(formularz.elements["f_nazwisko"], "Podaj nazwisko!", isWhiteSpaceOrEmpty)) return false;
+
+  if(!checkStringAndFocus(formularz.elements["f_kod"], "Podaj kod!", isWhiteSpaceOrEmpty)) return false;
+
+  if(!checkStringAndFocus(formularz.elements["f_ulica"], "Podaj ulicę!", isWhiteSpaceOrEmpty)) return false;
+
+  if(!checkStringAndFocus(formularz.elements["f_miasto"], "Podaj miasto!", isWhiteSpaceOrEmpty)) return false;
+
+  if(!checkStringAndFocus(formularz.elements["f_email"], "Podaj właściwy e-mail", isEmailInvalid)) return false;
+}
+
 function isWhiteSpaceOrEmpty(str) {
   return /^[\s\t\r\n]*$/.test(str);
 }
 
-function validate(formularz){
-  if(!checkStringAndFocus(formularz.elements["f_imie"], "Podaj imię!")) return false;
-
-  if(!checkStringAndFocus(formularz.elements["f_nazwisko"], "Podaj nazwisko!")) return false;
-
-  if(!checkStringAndFocus(formularz.elements["f_kod"], "Podaj kod!")) return false;
-
-  if(!checkStringAndFocus(formularz.elements["f_ulica"], "Podaj ulicę!")) return false;
-
-  if(!checkStringAndFocus(formularz.elements["f_miasto"], "Podaj miasto!")) return false;
-
-  if(!checkEmailAndFocus(formularz.elements["f_email"], "Podaj właściwy e-mail")) return false;
-}
-
-function checkStringAndFocus(obj, msg) {
-  let str = obj.value;
-  let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
-  if (isWhiteSpaceOrEmpty(str)) {
-    document.getElementById(errorFieldName).innerHTML = msg;
-    obj.focus();
-    return false;
-  }
-  else {
-    document.getElementById(errorFieldName).innerHTML = "";
-    return true;
-  }
-}
-
-function checkEmailAndFocus(obj, msg) {
-  let str = obj.value;
+function isEmailInvalid(str) {
   let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+  if (email.test(str))
+    return false;
+  else {
+    return true;
+  }
+}
+
+
+function checkStringAndFocus(obj, msg, func) {
+  let str = obj.value;
   let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
-  if (!email.test(str)) {
+  if (func(str)) {
     document.getElementById(errorFieldName).innerHTML = msg;
     obj.focus();
     return false;
@@ -43,4 +38,12 @@ function checkEmailAndFocus(obj, msg) {
     document.getElementById(errorFieldName).innerHTML = "";
     return true;
   }
+}
+
+function showElement(e) {
+  document.getElementById(e).style.visibility = 'visible';
+}
+
+function hideElement(e) {
+  document.getElementById(e).style.visibility = 'hidden';
 }
