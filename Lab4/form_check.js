d3654e6208019fe874e1b1b02de347e62be06843
Jakub-Biernat <k.biernat@vp.pl>
2026-04-03 14:09:50 +0200

    Add podpunkty 11-15

--- Lab4/form_check.js
+++ Lab4/form_check.js
@@ -3,10 +3,33 @@ function isWhiteSpaceOrEmpty(str) {
 }
 
 function validate(formularz){
-  if(isWhiteSpaceOrEmpty(formularz.elements["f_imie"].value)){
-    alert("Podaj imię!");
+  if(!checkString(formularz.elements["f_imie"].value, "Podaj imię!")) return false;
+
+  if(!checkString(formularz.elements["f_nazwisko"].value, "Podaj nazwisko!")) return false;
+
+  if(!checkString(formularz.elements["f_kod"].value, "Podaj kod!")) return false;
+
+  if(!checkString(formularz.elements["f_ulica"].value, "Podaj ulicę!")) return false;
+
+  if(!checkString(formularz.elements["f_miasto"].value, "Podaj miasto!")) return false;
+
+  if(!checkEmail(formularz.elements["f_email"].value)) return false;
+}
+
+function checkString(str, alertText){
+  if(isWhiteSpaceOrEmpty(str)){
+    alert(alertText);
     return false;
   }
   else return true;
+}
 
+function checkEmail(str) {
+  let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+  if (email.test(str))
+    return true;
+  else {
+    alert("Podaj właściwy e-mail");
+    return false;
+  }
 }
